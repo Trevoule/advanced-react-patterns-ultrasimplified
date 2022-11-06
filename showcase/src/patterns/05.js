c0cbf495c47976bb441d8063003f4dbb25d39690
Władysław Reginiewicz <wladyslaw.reginiewicz@gmail.com>
2022-11-06 15:39:47 +0100

    5.2 Controlling state of multiple components

--- showcase/src/patterns/05.js
+++ showcase/src/patterns/05.js
@@ -303,21 +303,49 @@ const CountTotal = ({ style: userStyles = {} }, className) => {
   );
 };
 
+const INITIAL_STATE = {
+  count: 0,
+  countTotal: 2100,
+  isClicked: false,
+};
+
+const MAXIMUM_CLAP_VALUE = 10;
+
 const Usage = () => {
-  const [count, setCount] = useState(0);
+  const [state, setState] = useState(INITIAL_STATE);
   const handleClap = (clapState) => {
-    setCount(clapState.count);
+    // setCount(clapState.count);
+    setState(({ count, countTotal }) => ({
+      count: Math.min(count + 1, MAXIMUM_CLAP_VALUE),
+      countTotal: count < MAXIMUM_CLAP_VALUE ? countTotal + 1 : countTotal,
+      isClicked: true,
+    }));
   };
 
   return (
     <div style={{ width: "100%" }}>
-      <MediumClap onClap={handleClap} className={userCustomStyles.clap}>
+      <MediumClap
+        values={state}
+        onClap={handleClap}
+        className={userCustomStyles.clap}
+      >
         <ClapIcon className={userCustomStyles.icon} />
         <ClapCount className={userCustomStyles.count} />
         <CountTotal className={userCustomStyles.total} />
       </MediumClap>
-      {!!count && (
-        <div className={styles.info}>{`You have clapped ${count} times!`}</div>
+      <MediumClap
+        values={state}
+        onClap={handleClap}
+        className={userCustomStyles.clap}
+      >
+        <ClapIcon className={userCustomStyles.icon} />
+        <ClapCount className={userCustomStyles.count} />
+        <CountTotal className={userCustomStyles.total} />
+      </MediumClap>
+      {!!state.count && (
+        <div
+          className={styles.info}
+        >{`You have clapped ${state.count} times!`}</div>
       )}
     </div>
   );
